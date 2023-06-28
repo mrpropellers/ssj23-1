980d1768e3bc05d068fa0b77d3fcc0601baa80e7
Devin Miller <devook@gmail.com>
2023-06-27 18:45:00 -0700

    Tweak lighting to darken night, brighten spirits; Add starmap background; Paint terrain

--- SSJ23-Idle/Assets/Scripts/Clock/DayNightManager.cs
+++ SSJ23-Idle/Assets/Scripts/Clock/DayNightManager.cs
@@ -8,12 +8,19 @@ namespace LeftOut.GameJam.Clock
     {
         Vector3 m_InitialUp;
         Quaternion m_InitialOrientation;
+        float m_LastProgressValue;
+        bool m_SunShadowsOn;
+        
+        [SerializeField]
+        LightShadows AtmosphereShadows = LightShadows.Hard;
 
         [SerializeField]
         Gradient m_SunColorGradient;
 
         [SerializeField]
         Light m_Sun;
+        [SerializeField]
+        Light m_Moon;
         
         [FormerlySerializedAs("dayNight")]
         public GameObject Atmosphere;
@@ -24,14 +31,13 @@ namespace LeftOut.GameJam.Clock
             m_InitialUp = Atmosphere.transform.up;
             m_InitialOrientation = Atmosphere.transform.rotation;
             m_Sun.color = m_SunColorGradient.Evaluate(0);
+            m_Moon.shadows = LightShadows.None;
+            m_Sun.shadows = AtmosphereShadows;
+            m_SunShadowsOn = true;
         }
 
         void Update()
         {
-            if (!PomoTimer.IsPlaying)
-            {
-                return;
-            }
             ApplyTimerUpdate();
         }
 
@@ -41,17 +47,39 @@ namespace LeftOut.GameJam.Clock
             // 0 => session just started
             // 1 => session is ending
             var progress = PomoTimer.ProgressThroughSession;
+            if (Mathf.Approximately(progress, m_LastProgressValue))
+            {
+                return;
+            }
+
+            m_LastProgressValue = progress;
             // This is an enum that tells you what type of session the user is currently doing
             // Right now we can just assume if they're focusing, it's daytime, and breaks happen at night
             // Add phase offset if it's nighttime
             var session = PomoTimer.currentSessionType;
             if (session == SessionType.Focus)
             {
+                // This is roughly when the horizon switches...
+                if (m_SunShadowsOn && progress > 0.95f)
+                {
+                    m_Sun.shadows = LightShadows.None;
+                    m_Moon.shadows = AtmosphereShadows;
+                    m_SunShadowsOn = false;
+                }
                 m_Sun.color = m_SunColorGradient.Evaluate(progress);
             }
             else
             {
+
+                if (!m_SunShadowsOn && progress > 0.95f)
+                {
+                    m_Moon.shadows = LightShadows.None;
+                    m_Sun.shadows = AtmosphereShadows;
+                    m_SunShadowsOn = true;
+                }
+
                 progress += 1f;
+
                 // Ensure the sun gets set back to start-of-day color after it's definitely set, but before it next rises
                 if (progress > 1.5f)
                 {
